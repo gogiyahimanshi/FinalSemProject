fbbd7d442185829a4d38063951fc5f54d4034784
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-09-02 10:02:56 +0530

    Update ecr.tf

--- stacks/ecr.tf
+++ stacks/ecr.tf
@@ -1,6 +1,7 @@
-provider aws {
-  version = ~ 2.0
-  region  = eu-west-2 # Setting my region to London. Use your own region here
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA5ATX6WSWCD5W46HL"
+  secret_key = "Kj2Ch/uiIttguXxNxx48/VRscAS1knsYJJemP9I4"
 }
 
 resource aws_ecr_repository my_first_ecr_repo {
