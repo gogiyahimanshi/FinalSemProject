149b8674daeb426f0f49dfa86ff9d9e8227c1a98
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-09-04 13:35:01 +0530

    Update providers.tf

--- workspaces/providers.tf
+++ workspaces/providers.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5ATX6WSWCD5W46HL"
-  secret_key = "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"
+  access_key = "AKIA5ATX6WSWA57OHRNK"
+  secret_key = "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"
 }
-
-
